ab319e6a21be5223df3d07a115f7bab9a2b1a6cc
Daman Chhina <dpschhina@gmail.com>
2022-10-02 18:04:44 -0400

    Add listItems

--- src/store/CsvBasedItemStore.java
+++ src/store/CsvBasedItemStore.java
@@ -2,7 +2,18 @@ package src.store;
 
 import src.data.Item;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
 public class CsvBasedItemStore implements ItemStore {
+    private File file;
+
+    public CsvBasedItemStore(String path) {
+        // Need to close resources
+        this.file = new File(path);
+    }
+
     @Override
     public Item addItem(int price, String content) {
         return null;
@@ -11,5 +22,10 @@ public class CsvBasedItemStore implements ItemStore {
     @Override
     public int removeItem(int id) {
         return 0;
+    }
+
+    @Override
+    public List<Item> listItems() {
+        return Collections.emptyList();
     }
 }
--- src/store/ItemStore.java
+++ src/store/ItemStore.java
@@ -2,7 +2,10 @@ package src.store;
 
 import src.data.Item;
 
+import java.util.List;
+
 public interface ItemStore {
     Item addItem(int price, String content);
     int removeItem(int id);
+    List<Item> listItems();
 }
