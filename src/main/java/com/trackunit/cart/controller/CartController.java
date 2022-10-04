fabfd0c9ca16f631bb636c91f3bd6feee591a84d
Daman Chhina <dpschhina@gmail.com>
2022-10-04 01:22:04 -0400

    Add cart controller

---
+++ src/main/java/com/trackunit/cart/controller/CartController.java
@@ -0,0 +1,24 @@
+package com.trackunit.cart.controller;
+
+import com.trackunit.cart.api.CartApi;
+import com.trackunit.cart.data.CartRepository;
+import com.trackunit.cart.data.ItemRepository;
+import com.trackunit.cart.model.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CartController implements CartApi {
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public ResponseEntity<Integer> createCart() {
+        Cart cart = cartRepository.save(new Cart());
+        return new ResponseEntity(cart.getId(), HttpStatus.OK);
+    }
+}
